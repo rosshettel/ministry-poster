e8e78d2fcd1ac188921bbc71175c48105cddb2bb
Ross Hettel <rhettel@highground.com>
2015-07-27 13:21:30 -0500

    quick server standup

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    server = app.listen(port);
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function (req, res) {
+    res.send(__dirname + '/static/index.html');
+});
+
+app.post('/', function (req, res) {
+    //get webhook
+    //check if user_name is on approved list
+        //if not, respond with message to user
+        //if so, send empty 200 response
+            //then send message to webhook, formatted as ministry
+});
