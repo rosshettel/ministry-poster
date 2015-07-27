1b3afe50f48c93d93359056617b8c73b9e1c4afa
Ross Hettel <rhettel@highground.com>
2015-07-27 13:55:35 -0500

    added posting as ministry

--- server.js
+++ server.js
@@ -2,23 +2,44 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     app = express(),
     port = process.env.PORT || 3000,
-    server = app.listen(port);
+    server = app.listen(port),
+    superagent = require('superagent');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function (req, res) {
     res.send(__dirname + '/static/index.html');
 });
 
-function isValidToken (payload) {
+function isValidToken(payload) {
     var token = process.env.TOKEN;
     return payload.token === token;
 }
 
-function isApprovedUser (payload) {
+function isApprovedUser(payload) {
     var approvedUsers = ['ross', 'itsmerossw', 'libby', 'moose'];
     return approvedUsers.indexOf(payload.user_name) !== -1;
+}
+
+function postMinistryMessage(payload, callback) {
+    var message = {
+            username: 'Ministry of Food and Drink',
+            icon_url: 'http://ministry-poster.herokuapp.com/ministry_logo.jpg',
+            channel: payload.channel_name,
+            text: payload.text
+        },
+        webhook = process.env.WEBHOOK;
+    superagent.post(webhook, message, function (err, res) {
+        if (err) {
+            return callback(err);
+        }
+        if (res.status !== 200) {
+            console.log('Returned non 200 status code', res.body);
+            return callback('Received' + res.status + ' response');
+        }
+        callback();
+    })
 }
 
 app.post('/', function (req, res) {
@@ -26,14 +47,15 @@ app.post('/', function (req, res) {
 
     if (isValidToken(payload)) {
         if (isApprovedUser(payload)) {
-            res.send('You are an approved Ministry member.');
+            postMinistryMessage(payload, function (err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(200).send('You are an approved Ministry member. :disaproval:');;
+                }
+            });
         } else {
-            res.send('Sorry - you are not an approved Ministry member.');
+            res.send('Sorry - you are not an approved Ministry member. :disaproval:');
         }
     }
-    //get webhook
-    //check if user_name is on approved list
-        //if not, respond with message to user
-        //if so, send empty 200 response
-            //then send message to webhook, formatted as ministry
 });
